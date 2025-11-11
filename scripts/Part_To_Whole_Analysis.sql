683687454b7a65ea0d0be94a91597e0d0fef4b84
Amine Assatah <assatahamine@gmail.com>
2025-11-11 03:03:51 +0100

    Create Part_To_Whole_Analysis.sql

---
+++ scripts/Part_To_Whole_Analysis.sql
@@ -0,0 +1,29 @@
+/* Part to whole 
+Analyze how an individual part is performing compared to the overall , 
+allowing us to understand which category has the greatest impact on the business
+
+*/
+
+-- which categories contribute the most to overall sales ?
+
+WITH category_sales AS (
+SELECT
+category , 
+SUM(sales_amount) total_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+on p.product_key = f.product_key
+GROUP BY category )
+
+SELECT 
+category,
+total_sales,
+SUM(total_sales) OVER() overall_sales ,
+CONCAT(ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER())*100 , 2), '%') AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC
+
+
+
+
+
